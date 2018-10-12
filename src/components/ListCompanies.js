83f9f1444567d9f5f4f6a9924386c90211aa491e
Maggie Rose <maggie.rose.g@gmail.com>
2018-10-11 20:35:33 -0500

    setup query to fetch company data from GQL endpoint

--- src/components/ListCompanies.js
+++ src/components/ListCompanies.js
@@ -1,19 +1,32 @@
 import React from 'react'
 import Company from './Company.js'
+import { Query } from 'react-apollo'
+import gql from 'graphql-tag'
 
+
+const COMPANY_QUERY = gql`{
+                          allCompanies {
+                            id,
+                            name
+                          }
+                        }`
 class ListCompanies extends React.Component{
   render (){
-    const companies = [{
-                        name: "Dunder Mifflin",
-                        description: "This is a paper company"
-                      },
-                      {
-                        name: "Paddy's Pub",
-                        description: "This is a bar"
-                      }]
-    return(<div>
-      {companies.map(company => <Company key={company.name} {...company} />) }
-    </div>)
+
+    return(<Query query={COMPANY_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>Fetching</div>
+        if (error) return <div>Error</div>
+
+        const companies = data.allCompanies;
+
+        return (
+          <div>
+            {companies.map(company => <Company key={company.name} {...company} />) }
+          </div>
+        )
+      }}
+    </Query>)
    }
 }
 
