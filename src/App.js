a0bd0b2044244d254cb52743d791dd7e05526a95
Maggie Rose Fischer <maggie.rose.g@gmail.com>
2019-01-15 10:02:02 -0600

    survey

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import ListCompanies from './components/ListCompanies.js';
+import TextSignUp from './components/TextSignUp.js';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <ListCompanies />
+        <TextSignUp onClick={console.log}/>
       </div>
     );
   }
---
+++ src/components/TextSignUp.js
@@ -0,0 +1,21 @@
+import React from 'react'
+class TextSignUp extends React.Component{
+	constructor(props){
+		super()
+	}	
+	render(){ 
+		return(
+			<div>
+				<label>Enter Your Phone Number</label>
+				<input ref="phone"/>
+				<input type="submit" onClick={this.submitPhoneNumber.bind(this)}/>
+			</div>
+		)
+	}
+
+	submitPhoneNumber(){
+		this.props.onClick(this.refs.phone.value)
+	}
+}
+
+export default TextSignUp
